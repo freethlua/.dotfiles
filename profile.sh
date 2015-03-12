66c6c30c69627af789984246fce629528dddf5a1
x <x>
2015-03-12 21:33:09 +0530

    .

--- profile.sh
+++ profile.sh
@@ -7,11 +7,11 @@
 # or
 # if [[ -t 0 ]];then curl https://raw.githubusercontent.com/xxx/.dotfiles/master/.bashrc -s -o /tmp/temp.bashrc 2> /dev/null && . /tmp/temp.bashrc && rm /tmp/temp.bashrc; fi
 
-version=0.7.49c
-if [[ "$dotfilesbashrcversion0749c" == "true" ]];then
+version=0.7.50a
+if [[ "$dotfilesbashrcversion0750a" == "true" ]];then
     return
 else
-    dotfilesbashrcversion0749c="true"
+    dotfilesbashrcversion0750a="true"
 fi
 function .v(){
     # echo -e "\e[7m .dotfiles/.bashrc \e[0m \e[7m v$version \e[0m"
