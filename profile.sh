cb103065458c1146ef0f0299e293ebb15675ac8a
x <x>
2015-04-09 22:06:04 +0530

    .

--- profile.sh
+++ profile.sh
@@ -7,11 +7,11 @@
 # or
 # if [[ -t 0 ]];then curl https://raw.githubusercontent.com/xxx/.dotfiles/master/.bashrc -s -o /tmp/temp.bashrc 2> /dev/null && . /tmp/temp.bashrc && rm /tmp/temp.bashrc; fi
 
-version=0.8.01a
-if [[ "$dotfilesbashrcversion0801a" == "true" ]];then
+version=0.8.02a
+if [[ "$dotfilesbashrcversion0802a" == "true" ]];then
     return
 else
-    dotfilesbashrcversion0801a="true"
+    dotfilesbashrcversion0802a="true"
 fi
 function .v(){
     # echo -e "\e[7m .dotfiles/.bashrc \e[0m \e[7m v$version \e[0m"
@@ -205,6 +205,7 @@ function .v(){
     # node .
     function node(){
         if [[ -f ./app.js ]]; then local file="app.js"; fi
+        if [[ -f ./server.js ]]; then local file="server.js"; fi
         if [[ -f ./index.js ]]; then local file="index.js"; fi
         if [[ -n $file ]]; then
             echo -e "Running $file $@\n=======\n"
